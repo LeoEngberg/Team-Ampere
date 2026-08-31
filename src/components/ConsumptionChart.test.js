b64cc7fa1bb679517f55bd48d89b0a88a1659f2f
LeoEngberg <leo.engberg@gmail.com>
2026-08-31 15:19:48 +0200

    Added two new tests (component test for ConsumptionChart and e2e test for Login and merge into main

---
+++ src/components/ConsumptionChart.test.js
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect, vi } from 'vitest'
+import ConsumptionChart from './ConsumptionChart.vue'
+
+const ChartMock = vi.hoisted(() => {
+  return vi.fn(function () {
+    this.destroy = vi.fn()
+  })
+})
+
+vi.mock('chart.js/auto', () => ({
+  default: ChartMock
+}))
+
+describe('ConsumptionChart', () => {
+  it('skickar rätt data till Chart.js', () => {
+    mount(ConsumptionChart, {
+      props: {
+        months: ['Jan', 'Feb', 'Mar'],
+        values: [100, 200, 300]
+      }
+    })
+
+    expect(ChartMock).toHaveBeenCalled()
+
+    const config = ChartMock.mock.calls[0][1]
+
+    expect(config.data.labels).toEqual([
+      'Jan',
+      'Feb',
+      'Mar'
+    ])
+
+    expect(config.data.datasets[0].data).toEqual([
+      100,
+      200,
+      300
+    ])
+  })
+})
