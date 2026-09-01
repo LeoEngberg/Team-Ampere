41279c1435723ec04a21f7d97482248ca3f1d2e6
rabbiyawaqar2011@gmail.com <142407500+Rabbit-89@users.noreply.github.com>
2026-09-01 14:31:47 +0200

    test to validate if the inputfields in MoveForm are filled

---
+++ src/utils/validateMove.js
@@ -0,0 +1,14 @@
+
+export const validateMove = (moveForm) => {
+    return{
+        address: moveForm.address.length > 0,
+        zip: moveForm.zip.length === 5,
+        city: moveForm.city.length > 0,
+        date: moveForm.date.length > 0,
+        contract: moveForm.contract.length > 0
+    };
+
+}
+
+
+// const validationResult = { address: false, zip: false, city: false, moveDate: false, contract: false };
---
+++ src/utils/validateMove.test.js
@@ -0,0 +1,30 @@
+import {describe, it, expect} from 'vitest';
+import {validateMove} from './validateMove.js';
+
+describe('validateMove', () => {
+
+    it ('should return invalid if address is missing', () => {
+        //Arrange
+            const moveForm = { address: '', zip: '12345', city: 'Stockholm', date: '2024-07-01', contract: 'Rörligt pris' };
+            
+            const expected = {
+                address: false,
+                zip: true,
+                city: true,
+                date: true,
+                contract: true
+            };
+
+            //Act
+            const result = validateMove(moveForm, new Date());
+            
+            //Assert
+            expect(result).toStrictEqual(expected);
+    })
+})
+
+// postnummer 5 siffror
+// flyttdatum i framtiden
+// flyttdatum format ÅÅÅÅÅ-MM-DD
+// minst 14 dagar fram i tiden
+// exakt 14 dagar okej, 13 inte okej
